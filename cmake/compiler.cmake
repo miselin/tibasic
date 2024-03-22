e6c1e6925ab3dafeb967b074ba9c33a269e1c45c
Matt Iselin <matthew@repl.it>
2024-03-22 11:10:18 -0700

    Replace Makefile and VS .sln files with CMake

---
+++ cmake/compiler.cmake
@@ -0,0 +1,45 @@
+add_library(cmake_cpp_tibasic_compiler_options INTERFACE)
+
+option(WARNINGS_ARE_ERRORS "Disallow warnings by treating them as errors" ON)
+option(ASAN "Enable address sanitizer" OFF)
+option(MSAN "Enable memory sanitizer" OFF)
+option(TSAN "Enable thread sanitizer" OFF)
+option(UBSAN "Enable undefined behavior sanitizer" OFF)
+
+if (MSVC)
+  target_compile_options(cmake_cpp_boilerplate_compiler_options INTERFACE /W4 "/permissive-")
+
+  if (WARNINGS_ARE_ERRORS)
+    target_compile_options(cmake_cpp_tibasic_compiler_options INTERFACE /WX)
+  endif ()
+else ()
+  target_compile_options(cmake_cpp_tibasic_compiler_options INTERFACE
+    -Wall -Wextra -Wpedantic -Wundef -Wshadow -Wpointer-arith -Wcast-align -Wstrict-prototypes -Wstrict-overflow=5
+    -Wwrite-strings -Waggregate-return -Wcast-qual -Wswitch-default -Wswitch-enum -Wconversion -Wunreachable-code
+    -Wformat=2 -Winit-self)
+  target_compile_definitions(cmake_cpp_tibasic_compiler_options INTERFACE _POSIX_C_SOURCE=200809L _XOPEN_SOURCE=700 _DEFAULT_SOURCE)
+
+  if (WARNINGS_ARE_ERRORS)
+    target_compile_options(cmake_cpp_tibasic_compiler_options INTERFACE -Werror)
+  endif ()
+endif ()
+
+if (ASAN)
+  target_compile_options(cmake_cpp_tibasic_compiler_options INTERFACE -fsanitize=address -fno-omit-frame-pointer)
+  target_link_libraries(cmake_cpp_tibasic_compiler_options INTERFACE -fsanitize=address)
+endif ()
+
+if (MSAN)
+  target_compile_options(cmake_cpp_tibasic_compiler_options INTERFACE -fsanitize=memory -fno-omit-frame-pointer)
+  target_link_libraries(cmake_cpp_tibasic_compiler_options INTERFACE -fsanitize=memory)
+endif ()
+
+if (TSAN)
+  target_compile_options(cmake_cpp_tibasic_compiler_options INTERFACE -fsanitize=thread)
+  target_link_libraries(cmake_cpp_tibasic_compiler_options INTERFACE -fsanitize=thread)
+endif ()
+
+if (UBSAN)
+  target_compile_options(cmake_cpp_tibasic_compiler_options INTERFACE -fsanitize=undefined)
+  target_link_libraries(cmake_cpp_tibasic_compiler_options INTERFACE -fsanitize=undefined)
+endif ()
